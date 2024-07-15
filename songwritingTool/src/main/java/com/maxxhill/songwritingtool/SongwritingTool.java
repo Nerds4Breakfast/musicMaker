2ff8d0c3273b8615b3f44a1b27bf6782a55fa25d
Matthew C Montanio <71164806+Nerds4Breakfast@users.noreply.github.com>
2024-07-14 19:19:47 -0500

    Add files via upload

---
+++ songwritingTool/src/main/java/com/maxxhill/songwritingtool/SongwritingTool.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.maxxhill.songwritingtool;
+
+import hooks.SongwritingToolGUI;
+
+/**
+ *
+ * @author Matt
+ */
+public class SongwritingTool {
+
+    public static void main(String[] args) {
+        
+    }
+}
